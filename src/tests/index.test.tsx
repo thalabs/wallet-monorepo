635682f110fa33903102c6a4d8cb6c6113b2761e
amrro <amr.elghobary@proton.me>
2024-05-23 20:26:06 +0300

    Fix dummy test

--- src/tests/index.test.tsx
+++ src/tests/index.test.tsx
@@ -2,13 +2,13 @@
  * @jest-environment jsdom
  */
 import {render, screen} from "@testing-library/react";
-import Home from "@/app/page";
+import Home from '@/src/app/page';
 
 
 describe("Home", () => {
     it("renders a heading", () => {
         render(<Home/>);
 
-        expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument();
+        expect(screen.getByText(/Make your/i)).toBeInTheDocument();
     });
 });
