b01d846b8cb7257a8950530bf943bbe76e2209c1
Amr Elghobary <12991700+amrro@users.noreply.github.com>
2024-05-23 21:28:08 +0300

    Add Home Page Scaffolding (#3)
    * add Home Screen
    * Utilizes the [NextUI](https://nextui.org/) UI library.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,77 @@
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+} from '@nextui-org/navbar';
+import { Button } from '@nextui-org/button';
+import { Link } from '@nextui-org/link';
+import { link as linkStyles } from '@nextui-org/theme';
+import NextLink from 'next/link';
+import clsx from 'clsx';
+
+
+import {
+  GithubIcon, KeyIcon,
+  Logo,
+} from '@/src/components/icons';
+import { siteConfig } from '@/src/config/site';
+
+export const Navbar = () => {
+
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Logo />
+            <p className="font-bold text-inherit">Tha Wallet</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: 'foreground' }),
+                  'data-[active=true]:text-primary data-[active=true]:font-medium',
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden md:flex">
+          <Button
+            isExternal
+            as={Link}
+            className="text-sm font-normal text-default-600 bg-default-100"
+            href={siteConfig.links.sponsor}
+            startContent={<KeyIcon className="text-danger" />}
+            variant="flat"
+            color="danger"
+          >
+            Authenticate
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+          <GithubIcon className="text-default-500" />
+        </Link>
+        <NavbarMenuToggle />
+      </NavbarContent>
+    </NextUINavbar>
+  );
+};
