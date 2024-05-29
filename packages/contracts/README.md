8e625e0e7aacf878ad35e87edfc52264c1d4088b
hz <13457698+hozzjss@users.noreply.github.com>
2024-05-29 19:10:08 +0300

    Revert "add contracts to monorepo"

--- packages/contracts/README.md
+++
@@ -1,3 +0,0 @@
-# Smart Account contracts
-
-This repo will include the smart contracts for the smart account system.
--- packages/contracts/tests/dummy-caller-ext.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("dummy caller", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
-});
