131db320c244648e476215aef8d2c758d0eecb71
hz <gravatar.m2a16@simplelogin.com>
2024-06-03 15:31:13 +0300

    style: add prettier commands to format

--- apps/website/.eslintrc.cjs
+++ apps/website/.eslintrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-	root: true,
-	extends: ["@repo/eslint-config/react"]
+  extends: ["@repo/eslint-config/react"],
+  root: true,
 };
--- packages/contracts/vitest.config.js
+++ packages/contracts/vitest.config.js
@@ -1,8 +1,10 @@
-
 /// <reference types="vitest" />
 
 import { defineConfig } from "vite";
-import { vitestSetupFilePath, getClarinetVitestsArgv } from "@hirosystems/clarinet-sdk/vitest";
+import {
+  vitestSetupFilePath,
+  getClarinetVitestsArgv,
+} from "@hirosystems/clarinet-sdk/vitest";
 
 /*
   In this file, Vitest is configured so that it works seamlessly with Clarinet and the Simnet.
@@ -39,4 +41,3 @@ export default defineConfig({
     },
   },
 });
-
